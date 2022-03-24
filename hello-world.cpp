dc513f4fe4d00055a0aac162cc7ef4cf52f73281
arhh <arhed7@yahoo.com>
2022-03-24 20:56:38 +0000

    add hello-world source file

---
+++ hello-world.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Hello, world!" << endl;
+
+    cout << "Enter your name: " << endl;
+
+    cout << INT_MAX << endl;
+    cout << INT_MIN << endl;
+    // cin >> name;
+    //
+    // cout << "You entered: " << name << endl;
+    return 0;
+}
